bef4037d7939f08c4f46acdd173f749702a0f628
ebenlembe <ebnlembe@hotmale.com>
2022-12-21 18:19:58 -0800

    fix wrong identifier

--- src/getChannelStreams.ts
+++ src/getChannelStreams.ts
@@ -7,7 +7,7 @@ export default async function getChannelStreams(id: string, published_after?: Da
   try {
     const data: any = await getData('https://m.youtube.com/channel/'+id+'/streams')
     const apikey = data.apikey
-    let tabs: any[] = data.contents.singleColumnBrowseResultsRenderer.tabs.filter((t) => t.tabRenderer.title === 'Live')
+    const tabs: any[] = data.contents.singleColumnBrowseResultsRenderer.tabs.filter((t) => t.tabRenderer.title === 'Live')
     if (!tabs || !tabs.length) {
       throw new Error('No livestreams found')
     }
